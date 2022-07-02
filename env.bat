bcc658d419af4bfc77bed95373ce5693a5ad6a23
nicguzzo <nicguzzo@gmail.com>
2022-07-02 19:13:07 -0300

    updated architectury

--- env.bat
+++ env.bat
@@ -1,2 +1,2 @@
 SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_291\
-SET PATH=%PATH%;C:\Program Files\Java\jdk1.8.0_291\bin
+SET PATH=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Program Files\Java\jdk1.8.0_291\bin
