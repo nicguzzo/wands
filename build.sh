a9a118fe80b0bd0b04d7fadba184c42b2fe41307
nicguzzo <nicguzzo@gmail.com>
2022-07-13 00:28:22 -0300

    build log

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mcvers=(1.16.5 1.17.1 1.18.1 1.18.2 1.19)
 for v in ${mcvers[@]}; do  
   pushd wands$v
-      ./gradlew clean || exit
-      ./gradlew build || exit
+      ./gradlew clean >>../../build_log || exit
+      ./gradlew build >>../../build_log|| exit
   popd
 done
