cbf2410f56dbc10728644a8914f0a25aa717a755
nicguzzo <nicguzzo@gmail.com>
2022-07-14 19:42:16 -0300

    updated change versions script

--- change_versions.sh
+++ change_versions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-VERSION=2.6_beta
-
+VERSION=2.6
+TYPE=beta
 mcvers=(1.16.5 1.17.1 1.18.1 1.18.2 1.19)
 for v in ${mcvers[@]}; do  
   sed -i "s/mod_version=.*/mod_version=$VERSION/" wands$v/gradle.properties
+  sed -i "s/release_type=.*/release_type=$TYPE/" wands$v/gradle.properties
 done
