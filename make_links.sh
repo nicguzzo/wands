bccb542da11bed96c0545bc829c7639de4a5cc56
nicguzzo <nicguzzo@gmail.com>
2022-08-05 17:40:26 -0300

    links to forge/fabric build.gradle

--- make_links.sh
+++ make_links.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-mcvers=(1.16.5 1.17.1 1.18.1 1.18.2 1.19)
+mcvers=(1.16.5 1.17.1 1.18.1 1.18.2 1.19 1.19.1)
 for v in ${mcvers[@]}; do  
   pushd wands$v
     mkdir -p "common/src/main/java/net/nicguzzo"
+    pushd "common"
+      rm build.gradle
+      ln -s ../../common/build.gradle
+    popd
     pushd "common/src/main"
       pushd "java/net/nicguzzo"
-        ln -s ../../../../../../../src wands
+        rm wands
+        ln -s ../../../../../../../common/src wands
       popd
       pushd "resources"
+        rm assets
+        rm data
         ln -s  ../../../../../assets
         ln -s  ../../../../../data
       popd
     popd
     mkdir -p "fabric/src/main/java/net/nicguzzo/wands"    
+    pushd "fabric/"
+      rm build.gradle
+      ln -s  ../../fabric/build.gradle
+    popd
     pushd "fabric/src/main/java/net/nicguzzo/wands"
-      ln -s  ../../../../../../../../fabric
+      rm fabric
+      ln -s  ../../../../../../../../fabric/src fabric
     popd
     mkdir -p "forge/src/main/java/net/nicguzzo/wands"
-    pushd "forge/src/main/java/net/nicguzzo/wands"
-      ln -s  ../../../../../../../../forge      
+    pushd "forge/"      
+      rm build.gradle
+      ln -s  ../../forge/build.gradle
+    popd
+    pushd "forge/src/main/java/net/nicguzzo/wands"      
+      rm forge
+      ln -s  ../../../../../../../../forge/src forge      
     popd
   popd
 done
