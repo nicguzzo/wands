c5a70de7efe3c15093871a7b2223f0588089c0a5
Jeremy Epling <jeremy.epling@gmail.com>
2026-01-26 18:09:32 -0500

    fix: add missing INCSELBLOCK mode mappings for PASTE and TUNNEL
    PASTE and TUNNEL modes use INCSELBLOCK but weren't configured in FLAG_MODES.

--- common/src/main/java/net/nicguzzo/wands/wand/WandProps.java
+++ common/src/main/java/net/nicguzzo/wands/wand/WandProps.java
@@ -619,7 +619,7 @@ public class WandProps {
         Map.entry(Flag.EVEN, EnumSet.of(Mode.CIRCLE)),
         Map.entry(Flag.DIAGSPREAD, EnumSet.of(Mode.AREA)),
         Map.entry(Flag.MATCHSTATE, EnumSet.of(Mode.AREA, Mode.VEIN)),
-        Map.entry(Flag.INCSELBLOCK, EnumSet.of(Mode.FILL, Mode.LINE, Mode.CIRCLE, Mode.COPY, Mode.SPHERE)),  // Two-click modes only
+        Map.entry(Flag.INCSELBLOCK, EnumSet.of(Mode.FILL, Mode.LINE, Mode.CIRCLE, Mode.COPY, Mode.SPHERE, Mode.PASTE, Mode.TUNNEL)),  // Two-click modes + paste/tunnel
         Map.entry(Flag.STAIRSLAB, EnumSet.allOf(Mode.class)),  // All modes
         Map.entry(Flag.RFILLED, EnumSet.of(Mode.FILL)),
         Map.entry(Flag.TARGET_AIR, EnumSet.of(Mode.ROW_COL, Mode.GRID, Mode.COPY, Mode.PASTE, Mode.TUNNEL, Mode.ROCK, Mode.LINE, Mode.CIRCLE, Mode.FILL, Mode.SPHERE)),
